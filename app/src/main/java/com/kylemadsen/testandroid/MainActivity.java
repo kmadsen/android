83755ce99a9fd64833ebcf659e1011b668e902c6
Kyle Madsen <kyle.mdsn@gmail.com>
2018-07-15 13:21:41 -0700

    add an animations activity

--- app/src/main/java/com/kylemadsen/testandroid/MainActivity.java
+++ app/src/main/java/com/kylemadsen/testandroid/MainActivity.java
@@ -9,28 +9,32 @@ import android.widget.FrameLayout;
 import com.kylemadsen.testandroid.ar.ArFragment;
 import com.kylemadsen.testandroid.ar.ArMainActivity;
 import com.kylemadsen.testandroid.ar.ArObjectReader;
+import com.kylemadsen.testandroid.audio.AnimationMainActivity;
 import com.kylemadsen.testandroid.gnsslogger.GnssMainActivity;
 import com.kylemadsen.testandroid.logger.L;
 
 public class MainActivity extends AppCompatActivity {
-
-    View gotoArButton;
-    View gotoGnssLoggerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        gotoArButton = findViewById(R.id.goto_ar_button);
+        View gotoArButton = findViewById(R.id.goto_ar_button);
         gotoArButton.setOnClickListener(view -> {
             Intent intent = new Intent(this, ArMainActivity.class);
             startActivity(intent);
         });
 
-        gotoGnssLoggerButton = findViewById(R.id.goto_gnss_button);
+        View gotoGnssLoggerButton = findViewById(R.id.goto_gnss_button);
         gotoGnssLoggerButton.setOnClickListener(view -> {
             Intent intent = new Intent(this, GnssMainActivity.class);
+            startActivity(intent);
+        });
+
+        View gotoAnimationsButton = findViewById(R.id.goto_animations_button);
+        gotoAnimationsButton.setOnClickListener(view -> {
+            Intent intent = new Intent(this, AnimationMainActivity.class);
             startActivity(intent);
         });
     }
