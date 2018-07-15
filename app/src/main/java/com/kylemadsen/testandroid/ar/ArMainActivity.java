39ca7a74033b56af34e2fb804d7355988079216d
Kyle Madsen <kyle.mdsn@gmail.com>
2018-07-15 12:59:22 -0700

    import the gnss logger

---
+++ app/src/main/java/com/kylemadsen/testandroid/ar/ArMainActivity.java
@@ -0,0 +1,50 @@
+package com.kylemadsen.testandroid.ar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+
+import com.kylemadsen.testandroid.R;
+import com.kylemadsen.testandroid.ViewGroupController;
+import com.kylemadsen.testandroid.logger.L;
+
+public class ArMainActivity extends AppCompatActivity {
+
+    ViewGroupController configurationController;
+    ViewGroupController statusController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ar_main_activity);
+
+        FrameLayout configurationLayout = findViewById(R.id.ar_configuration);
+        FrameLayout statusLayout = findViewById(R.id.ar_status);
+        configurationController = ViewGroupController.onCreate(configurationLayout);
+        statusController = ViewGroupController.onCreate(statusLayout);
+
+        ArFragment arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+
+        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+            L.i("onTapPlane");
+            L.i(ArObjectReader.read(hitResult));
+            L.i(ArObjectReader.read(plane));
+            L.i("motionEvent: %s", motionEvent.toString());
+
+            //float distance = hitResult.getDistance();
+            //Pose pose = hitResult.getHitPose();
+            //L.i("");
+            //Anchor anchor = hitResult.createAnchor();
+            //Trackable trackable = hitResult.getTrackable();
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        statusController.onDestroy();
+        configurationController.onDestroy();
+
+        super.onDestroy();
+    }
+}
