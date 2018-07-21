a42819ae3f1bcebdc057b970d0ec7bd8bff0e429
Kyle Madsen <kyle.mdsn@gmail.com>
2018-07-21 14:58:24 -0700

    render with transparent background and make sources visible

--- compass/src/main/java/com/kmadsen/compass/CompassGLSurfaceView.java
+++ compass/src/main/java/com/kmadsen/compass/CompassGLSurfaceView.java
@@ -1,19 +1,24 @@
 package com.kmadsen.compass;
 
 import android.content.Context;
+import android.graphics.PixelFormat;
 import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
 
 public class CompassGLSurfaceView extends GLSurfaceView {
 
-    private final CompassGLRenderer renderer;
+    public CompassGLSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
 
-    public CompassGLSurfaceView(Context context){
-        super(context);
-
-        // Create an OpenGL ES 2.0 context
         setEGLContextClientVersion(2);
+        renderWithTransparentBackground();
+    }
 
-        renderer = new CompassGLRenderer();
-        setRenderer(renderer);
+    private void renderWithTransparentBackground() {
+        setZOrderOnTop(true);
+        setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+        getHolder().setFormat(PixelFormat.RGBA_8888);
+        setRenderer(new CompassGLRenderer());
+        setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
     }
 }
--- compass/src/main/java/com/kmadsen/compass/CompassMainActivity.java
+++ compass/src/main/java/com/kmadsen/compass/CompassMainActivity.java
@@ -1,19 +1,17 @@
 package com.kmadsen.compass;
 
+import android.graphics.PixelFormat;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class CompassMainActivity extends AppCompatActivity {
 
-    private GLSurfaceView glSurfaceView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.compass_main_activity);
+        setContentView(R.layout.compass_main_activity);
 
-        glSurfaceView = new CompassGLSurfaceView(this);
-        setContentView(glSurfaceView);
+        CompassGLSurfaceView glView = findViewById(R.id.surface_view);
     }
 }
