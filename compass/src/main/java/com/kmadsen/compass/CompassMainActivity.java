a93580d743e6aefbc60895cdd8bdd49f2fec25a1
Kyle Madsen <kyle.mdsn@gmail.com>
2018-07-21 16:52:51 -0700

    add a mapbox map

--- compass/src/main/java/com/kmadsen/compass/CompassMainActivity.java
+++ compass/src/main/java/com/kmadsen/compass/CompassMainActivity.java
@@ -1,9 +1,11 @@
 package com.kmadsen.compass;
 
-import android.graphics.PixelFormat;
-import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
 public class CompassMainActivity extends AppCompatActivity {
 
@@ -13,5 +15,16 @@ public class CompassMainActivity extends AppCompatActivity {
         setContentView(R.layout.compass_main_activity);
 
         CompassGLSurfaceView glView = findViewById(R.id.surface_view);
+
+        MapView mapView = findViewById(R.id.mapbox_mapview);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+
+                // Customize map with markers, polylines, etc.
+
+            }
+        });
     }
 }
