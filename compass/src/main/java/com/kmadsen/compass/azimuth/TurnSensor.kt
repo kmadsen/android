7065396f06e780a9f49456eeb2a348d2fb8f2428
Kyle <kyle.mdsn@gmail.com>
2019-07-17 14:18:13 -0700

    Simplify turn degrees

--- compass/src/main/java/com/kmadsen/compass/azimuth/TurnSensor.kt
+++ compass/src/main/java/com/kmadsen/compass/azimuth/TurnSensor.kt
@@ -23,22 +23,22 @@ class TurnSensor(
             .withLatestFrom(androidSensors.observeRawSensor(Sensor.TYPE_GYROSCOPE),
                 BiFunction { gravityEvent: SensorEvent, gyroEvent: SensorEvent ->
                     val gravity = Vector3d(
-                        gravityEvent.values[0].toDouble(),
-                        gravityEvent.values[1].toDouble(),
-                        gravityEvent.values[2].toDouble())
+                        -gravityEvent.values[0].toDouble(),
+                        -gravityEvent.values[1].toDouble(),
+                        -gravityEvent.values[2].toDouble())
                     val gyro = Vector3d(
                         gyroEvent.values[0].toDouble(),
                         gyroEvent.values[1].toDouble(),
                         gyroEvent.values[2].toDouble())
-                    val rotated = rotateToGravityDown(gravity, gyro)
+                    val rotated = dot(gravity, gyro) / gravity.length()
                     if (lastTimeNanos != 0L) {
                         val deltaNanos = gyroEvent.timestamp - lastTimeNanos
                         val deltaSeconds = deltaNanos / 1000000000.0
-                        val integral = 0.5 * deltaSeconds * (rotated.z + lastTurnSample)
+                        val integral = 0.5 * deltaSeconds * (rotated + lastTurnSample)
                         turnDegrees += integral
                     }
                     lastTimeNanos = gyroEvent.timestamp
-                    lastTurnSample = rotated.z
+                    lastTurnSample = rotated
                     turnDegrees
             })
             .map {
@@ -61,23 +61,3 @@ fun Double.toNormalizedDegrees(): Double {
     return (this * 180.0 / PI + 360.0) % 360.0
 }
 
-private fun rotateToGravityDown(gravity: Vector3d, gyro: Vector3d): Vector3d {
-    gravity.normalize()
-
-    val q: Quaternion = if (gravity.x == 0.0 && gravity.y == 0.0 && gravity.z > 0.0) {
-        Quaternion(1.0, 0.0, 0.0, 0.0)
-    } else {
-        Quaternion(-gravity.y, gravity.x, 0.0, 1.0 - gravity.z)
-            .normalize()
-    }
-
-    val quaternionVector = q.vector3d()
-    val t = cross(quaternionVector, gyro)
-    t.multiply(2.0)
-    gyro.add(cross(quaternionVector, t))
-    t.multiply(q.w)
-
-    gyro.add(t)
-
-    return gyro
-}
