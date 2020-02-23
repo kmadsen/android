2210c324b4d314cdfdc64c8e0b2fc6b3507d8ce4
Kyle Madsen <>
2020-02-23 03:21:44 +0300

    Add nano second device clock

--- core/src/main/java/com/kylemadsen/core/time/DeviceClock.kt
+++ core/src/main/java/com/kylemadsen/core/time/DeviceClock.kt
@@ -2,6 +2,9 @@ package com.kylemadsen.core.time
 
 import android.os.Build
 import android.os.SystemClock
+import androidx.annotation.RequiresApi
+import java.time.Instant
+import java.util.concurrent.TimeUnit
 
 object DeviceClock {
     private lateinit var deviceBootTimeProvider: DeviceBootTimeProvider
@@ -30,5 +33,34 @@ object DeviceClock {
 
     fun delta(elapsedMillis: Long): Long {
         return elapsedMillis() - elapsedMillis
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun currentTimeNanosInstant(): Instant {
+        return currentTimeNanosInstant(System.currentTimeMillis(), SystemClock.elapsedRealtimeNanos())
+    }
+
+    /**
+     * This will take a regular millisecond device clock. Estimate the nano seconds
+     * until the next second. Giving a nano-second current time
+     */
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun currentTimeNanosInstant(currentTimeMillis: Long, bootTime: Long): Instant {
+        val bootTimeSecs = TimeUnit.NANOSECONDS.toSeconds(bootTime)
+        val bootTimeFullSeconds = TimeUnit.SECONDS.toNanos(bootTimeSecs)
+        val bootTimeNanos = bootTime - bootTimeFullSeconds
+
+        val deviceTimeNanos = TimeUnit.MILLISECONDS.toNanos(currentTimeMillis)
+        val deviceTimeSecs = TimeUnit.NANOSECONDS.toSeconds(deviceTimeNanos)
+        val deviceTimeFullSecs = TimeUnit.SECONDS.toNanos(deviceTimeSecs)
+        val deviceTimeSecsNanos = deviceTimeNanos - deviceTimeFullSecs
+
+        var alignNanoOffset = deviceTimeSecsNanos - bootTimeNanos
+        if (alignNanoOffset < 0) {
+            alignNanoOffset += TimeUnit.SECONDS.toNanos(1)
+        }
+
+        val currentTimeSecs = TimeUnit.MILLISECONDS.toSeconds(currentTimeMillis)
+        return Instant.ofEpochSecond(currentTimeSecs, alignNanoOffset)
     }
 }
